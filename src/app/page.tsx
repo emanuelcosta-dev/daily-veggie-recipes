ce9085901b34da14ba6b9d52608760b8712f7f60
Emanuel Costa <emanuelcosta28@outlook.com>
2024-01-16 19:54:24 +0000

    Adds avatar,badge and button componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,8 +30,8 @@ export default async function Home() {
     <main>
       <div className="grid grid-cols-3 gap-8">
         {recipes.map((recipe) => (
-          <Card key={recipe.id}>
-            <CardHeader>
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
               <div>
                 <CardTitle>{recipe.title}</CardTitle>
                 <CardDescription>{recipe.time} mins to cook</CardDescription>
@@ -40,7 +40,7 @@ export default async function Home() {
             <CardContent>
               <p>{recipe.description}</p>
             </CardContent>
-            <CardFooter>
+            <CardFooter className="flex justify-between">
               <button>View Recipe</button>
               {recipe.vegan && <p>Vegan</p>}
             </CardFooter>
