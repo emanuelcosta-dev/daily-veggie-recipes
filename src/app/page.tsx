5ca79caf98fd300252d6ee798fafb888e5f306de
Emanuel Costa <emanuelcosta28@outlook.com>
2024-01-16 21:24:47 +0000

    Adds card breakpoints for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
   const recipes = await getRecipes();
   return (
     <main>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="ml-4 mr-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {recipes.map((recipe) => (
           <Card key={recipe.id} className="flex flex-col justify-between">
             <CardHeader className="flex-row gap-4 items-center truncate">
